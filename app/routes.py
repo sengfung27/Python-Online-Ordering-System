62f3b60b6889dbc92251ad717bc004e8b66a0284
Seng Mun <sengfung27@hotmail.com>
2018-03-03 23:25:27 -0500

    added ordering route

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,10 @@
 from app import app, db
 from flask import render_template, flash, redirect, request, url_for
-from app.forms import LoginForm, RegistrationForm
+from app.forms import EditProfileForm, LoginForm, RegistrationForm
 from app.models import User
 from flask_login import login_required, current_user, login_user, logout_user
 from werkzeug.urls import url_parse
-
+from datetime import datetime
 
 @app.route('/')
 @app.route('/index')
@@ -61,3 +61,41 @@ def register():
 	return render_template('register.html', title='Register', form=form)
 		
 		
+@app.route('/user/<username>')
+@login_required
+def user(username):
+	user = User.query.filter_by(username=username).first_or_404()
+	posts = [
+		{'author': user, 'body': 'Test post #1'},
+		{'author': user, 'body': 'Test post #2'}
+	]
+	return render_template('user.html', user=user,posts=posts)
+
+@app.route('/order')
+def order():
+	
+	return render_template('order.html',title ='Order')
+
+@app.before_request
+def before_request():
+	if current_user.is_authenticated:
+		current_user.last_seen = datetime.utcnow()
+		db.session.commit()
+
+@app.route('/edit_profile', methods=['GET','POST'])
+@login_required
+def edit_profile():
+	form = EditProfileForm(current_user.username)
+	if form.validate_on_submit():
+		current_user.username = form.username.data
+		current_user.about_me = form.about_me.data
+		db.session.commit()
+		flash('Your changes have been saved')
+		return redirect(url_for('edit_profile'))
+
+	elif request.method == 'GET':
+		form.username.data = current_user.username
+		form.about_me.data = current_user.about_me
+
+	return render_template('edit_profile.html', title='Edit Profile', form=form)
+	
